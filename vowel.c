de07e6cc50cab1b2380dda4b53534b7201bfea2d
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-07-15 21:42:32 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -4,9 +4,9 @@ void main()
  char ch;
  printf("enter the vowels:");
  ch=getchar();
- if(ch==a||ch==e||ch==i||ch==o||ch==u)
-  printf("enter the vowels %c");
+ if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+  printf(" the vowels %c",ch);
  else
-  printf("enter the not vowels %c");
+  printf("enter the not vowels %c",ch);
 }
  
