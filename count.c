88c1b139d4e960863603f056929f8b9553a64914
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-07-15 23:23:32 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+ int num,i,count;
+ printf("enter the number");
+ scanf("%d",&num);
+ for(i=0;i<num;i++)
+ {
+   i=count;
+   printf("%d",count);
+   count++;
+ }
+ return 0;
+}
