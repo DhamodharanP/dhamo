862cb0a608b3975bac2e01554d012a5fa85e2e77
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-07-15 21:25:22 +0530

    Update even.c

--- even.c
+++ even.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+void main()
+{
+  int num;
+  printf("enter the number:");
+  scanf("%d",&num);
+  if(num%2==0)
+    printf("enter the even number");
+  else
+    printf("enter the odd number");
+}
