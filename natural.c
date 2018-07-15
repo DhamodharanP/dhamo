dbeaf7c6a985b1f1056d718b8b56cc99a094ae8c
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-07-15 22:49:35 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int num,i;
+    printf("enter the number:");
+    scanf("%d",&num);
+    for(i=0;i<num;i++)
+    {
+        printf("\n");
+        printf("enter the natural number %d",i);
+    
+    }
+    return 0; 
+}
