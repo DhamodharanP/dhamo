f24787036ec397ac81b86b04e7795e4b0153d17b
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-07-15 21:14:41 +0530

    Create saraswathi.c

---
+++ saraswathi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ int num;
+ printf("enter the number:");
+ scanf("%d",&num);
+ if(num<5)
+  printf("enter the positive numbers:");
+ else 
+ printf("enter the negative numbers:");
+}
