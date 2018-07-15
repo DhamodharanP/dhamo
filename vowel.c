20f886838055c6bc95aacb749f1110f42e0e78c4
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-07-15 21:37:05 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ char ch;
+ printf("enter the vowels:");
+ ch=getchar();
+ if(ch==a||ch==e||ch==i||ch==o||ch==u)
+  printf("enter the vowels %c");
+ else
+  printf("enter the not vowels %c");
+}
+ 
